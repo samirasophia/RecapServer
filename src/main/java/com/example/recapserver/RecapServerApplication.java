e5e3cf82ab7cd1c3c5facd8ac7d07924ac9bdfc5
samirasophia <samira.tesan@coop.ch>
2023-12-13 08:38:27 +0100

    Initial commit

---
+++ src/main/java/com/example/recapserver/RecapServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.recapserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecapServerApplication.class, args);
+	}
+
+}
